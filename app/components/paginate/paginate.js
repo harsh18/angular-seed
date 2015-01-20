a14dfb52f8e256e7dac036acab7e78e1e3036a4f
harsh18 <harsh.lamba181189@gmail.com>
2015-01-20 16:52:48 +0530

    removed consoles

--- app/components/paginate/paginate.js
+++ app/components/paginate/paginate.js
@@ -3,15 +3,10 @@
 angular.module('movieDB.paginate-filter',[])
 	.filter('paginate', function(){
 		return function(arr, currentPage, pageSize){
-			console.log(currentPage);
-			console.log(pageSize);
-			console.log('index at which node start deleting', (currentPage-1)*pageSize);
-			console.log('integer indicating the numbr of old pages to delete', currentPage*pageSize);
 			try{
 				return arr.slice((currentPage-1)*pageSize, currentPage*pageSize);
 			}catch(err){
 				return arr;
 			}
-			
 		}
 	})
